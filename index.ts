fb2bc38e9576ab8c1d78c54011a304b59f0dc9e5
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-05 19:37:02 -0600

    observables handle dom events

--- index.ts
+++ index.ts
@@ -19,12 +19,24 @@ import { allBooks, allReaders } from "./data";
 
 // allBooksObservable$.subscribe(book => console.log(book.title));
 
-let source1$ = of('hello', 10, true, allReaders[0].name);
-// source1$.subscribe(value => console.log(value));
+// let source1$ = of('hello', 10, true, allReaders[0].name);
+// // source1$.subscribe(value => console.log(value));
 
-let source2$ = from(allBooks);
-// source2$.subscribe(book => console.log(book.title));
+// let source2$ = from(allBooks);
+// // source2$.subscribe(book => console.log(book.title));
 
-concat(source1$, source2$)
-  .subscribe(value => console.log(value));
+// concat(source1$, source2$)
+//   .subscribe(value => console.log(value));
 
+let button = document.getElementById('readersButton');
+
+fromEvent(button, 'click')
+  .subscribe(event => {
+    console.log(event);
+
+    let readersDiv = document.getElementById('readers');
+
+    for(let reader of allReaders) {
+      readersDiv.innerHTML += reader.name + '<br />';
+    }
+  });
